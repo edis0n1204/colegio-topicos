e5914c4a725626d0ef5a792932bbba5835bcf5a1
Edison vaca rocha <edisito1204@gmail.com>
2019-05-21 23:37:05 -0400

    Adicionamos modelos y controladores apis

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('parents', 'ParentController');
+Route::resource('courses', 'CourseController');
+Route::resource('matters', 'MatterController');
