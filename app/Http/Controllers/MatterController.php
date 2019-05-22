c2a746b8713f3f54174f28d3cce7c5ed1d1b97bb
Edison vaca rocha <edisito1204@gmail.com>
2019-05-21 23:50:30 -0400

    Adicionando vistas de curso, padres y materia

--- app/Http/Controllers/MatterController.php
+++ app/Http/Controllers/MatterController.php
@@ -13,7 +13,7 @@ class MatterController extends Controller
      */
     public function index()
     {
-        //
+        return view('matters.index');
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,15 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('courses.index') }}">Cursos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('matters.index') }}">Materias</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('parents.index') }}">Padres</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
